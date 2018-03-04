0b729e09c666a566c0fb0446ed088e97095ea4de
Pavel Sidorenko <pavel_sidorenko@epam.com>
2018-03-04 11:11:57 +0300

    create listen server, add mongo

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(3001, () => {
+    console.log('Express server listening on port 3001');
+});
+
+app.get('/api', (req, res) => {
+    res.send('API is running');
+});
